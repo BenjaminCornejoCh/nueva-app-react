192718e086f73ab53210d72e84337749a357ec07
Benjamin <benjamin.cornejo1@unmsm.edu.pe>
2021-01-11 00:01:04 -0500

    Añadir array de cursos

--- src/Curso.jsx
+++ src/Curso.jsx
@@ -1,26 +1,40 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const Curso = () => (
-        <article className="s-shadow-bottom m">
-            <div className="s-ratio-16-9 img-container s-radius-tl s-radius-tr">
-                <img
-                    src="https://edteam-media.s3.amazonaws.com/courses/medium/bcb63ac0-5dc5-48b7-a111-9974a7fc3bcd.png"/>
-            </div>
-            <div className="s-bg-white s-pxy-2">
-                <h3>C# Desde Cero</h3>
-                <p className="s-mb-0">Diseña, estructura y administra bases de datos SQL y crea un sistema de
-                    facturación con el tío Alexys</p>
-            </div>
-            <footer className="s-cross-center s-bg-grey s-pxy-2 s-radius-br s-radius-bl">
-                <div className="s-10 s-mr-1">
-                    <div className="circle ">
-                        <img src="https://ed-grid.com/assets/img/alexys.jpg"/>
-                    </div>
+const Curso = ({nombre, imagen, precio, profesor, foto}) => (
+    <article className="s-shadow-bottom m">
+        <div className="s-ratio-16-9 img-container s-radius-tl s-radius-tr">
+            {imagen ? <img src={imagen} alt={nombre} /> : <h3>No hay imagen :'(</h3>} {/*Puede colocarse otra imagen por defecto*/}
+        </div>
+        <div className="s-bg-white s-pxy-2">
+            <h3>{nombre}</h3>
+            <p className="s-mb-0">Diseña, estructura y administra bases de datos SQL y crea un sistema de
+                facturación con el tío Alexys</p>
+        </div>
+        <footer className="s-cross-center s-bg-grey s-pxy-2 s-radius-br s-radius-bl">
+            <div className="s-10 s-mr-1">
+                <div className="circle ">
+                    <img src={foto} alt="Foto-profe"/>
                 </div>
-                <p className="s-mb-0">Prof. Alexys Lozada</p>
-                <div className="button s-to-right">S/20 soles</div>
-            </footer>
-        </article>
+            </div>
+            <p className="s-mb-0">{profesor}</p>
+            <div className="button s-to-right">{precio}</div>
+        </footer>
+    </article>
 )
+
+Curso.propTypes = {
+    title: PropTypes.string,
+    imagen: PropTypes.string,
+    precio: PropTypes.string,
+    profesor: PropTypes.string
+}
+
+Curso.defaultProps = {
+    title: 'No se encontró título',
+    imagen: 'No hay imagen',
+    precio: 'Curso gratis',
+    profesor: 'No hay profesor(a)'
+}
 
 export default Curso
